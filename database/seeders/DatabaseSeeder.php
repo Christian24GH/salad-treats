7c995feedd85aa4fb4bfd901166ef9afa3ceb354
rrAeroll <jamesaeroll008@gmail.com>
2025-10-24 22:36:17 +0800

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,6 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('12345678'),
         ]);
 
-<<<<<<< HEAD
         User::factory(10)->create([
             'role' => 'customer',
             'password' => bcrypt('12345678'),
@@ -35,22 +34,5 @@ class DatabaseSeeder extends Seeder
         Order::factory(50)->create();
         Inventory::factory(100)->create();
         Product::factory(10)->create();
-=======
-        Product::factory(15)->create();
-        User::create([
-            'name' => 'Christian',
-            'email' => 'loquezchristian@gmail.com',
-            'role'  => 'Customer',
-            'password' => '12345678',
-            'email_verified_at' => now(),
-        ]);
-        User::create([
-            'name' => 'Owner',
-            'email' => 'owner@gmail.com',
-            'role'  => 'Owner',
-            'password' => '12345678',
-            'email_verified_at' => now(),
-        ]);
->>>>>>> 5d35d11d510550b56319823741fc9de0e00bad9c
     }
 }
